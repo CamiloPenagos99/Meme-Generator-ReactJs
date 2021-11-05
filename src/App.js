ff6db95764e02192cec54a0f78d58fa00b142ad9
camilo penagos <penagoscamilo99@gmail.com>
2021-11-04 23:56:27 -0500

    Elementos del componente, uso de useState y onChange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  // Manejar los estados
+  const [descripcion, setDescripcion] = useState("Descripcion inicial")
+  const [chiste, setChiste] = useState("Chiste inicial")
+
+  const onChangeDesc = (event) =>{
+    setDescripcion(event.target.value)
+  }
+
+  const selectPicker = (
+    <select>
+      <option value="fire">Casa en llamas</option>
+      <option value="futu">Futurama</option>
+      <option value="hist">History channel</option>
+      <option value="matr">Matrix</option>
+      <option value="phil">Philosoraptor</option>
+      <option value="smar">Smart guy</option>
+    </select>
+  );
+
+  const cajaDesc = (
+    <input onChange={onChangeDesc} type="text" placeholder="Ingresa descripcion"></input>
+  )
+
+  const cajaRem = (
+    <input type="text" placeholder="Ingresa el chiste"></input>
+  )
+
+  const estructura = (
+   <div>
+     <span>{descripcion}</span>
+     <br></br>
+     <span>{chiste}</span>
+     <img src=""></img>
+   </div>
+  )
+
+  const boton = ( <button>Exportar</button>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Select picker de memes */}
+
+      {selectPicker}
+      <br></br>
+
+      {/* Input text primera linea */}
+      {cajaDesc}
+      <br></br>
+
+      {/* Input text segunda linea */}
+      {cajaRem}
+      <br></br>
+
+      {/* boton de exportar*/}
+      {boton}
+
+      {/* Estructura del meme*/}
+      {estructura}
+
     </div>
   );
 }
